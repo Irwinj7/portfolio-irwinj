dc61dff0036f54757edbb39ba267586e8444da42
“irwinj7” <Irwinjorge@pursuit.com>
2023-04-01 06:45:35 -0400

    fixed website bugs

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+
+
+
+
+// fucntion to send email
+
+// let userEmail = document.getElementById("email")
+
+// function sendEmail(){
+//     Email.send({
+//         Host : "smtp.elasticemail.com",
+//         Username : "username",
+//         Password : "password",
+//         To : 'them@website.com',
+//         From : `${userEmail}`,
+//         Subject : `Contact Form `,
+//         Body : "And this is the body"
+//     }).then(
+//       message => alert(message)
+//     );
+// }
